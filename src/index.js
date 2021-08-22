703a5f5f3b21752ca2b8ff0ad5bc4834cab4548a
Yadirasarmientop <yadirasarmientop@gmail.com>
2021-08-21 21:43:57 -0500

    Servidor en funcionamiento

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@
 //gracias a este archivo vamos a arrancar nuestro servidor
 
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const app = express(); // servidor
+const {mongoose} = require('./models/database');
 
-app.listen(3000,()=>{
-    console.log('server on port 3000')
 
+//Settings ==> Seccion de configuración 
+app.set('port', process.env.port || 3000);
+
+
+//MiddLewares ==> funciones que se ejecutan antes de que lleguen a las rutas
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+//Routes ==> rutas o urls que podrá tener el servidor 
+app.use('/api/tasks', require('./routes/task_routes'));
+
+
+//Static files ==> archivos estaticos, es para decirle a express en donde irán mis archivos estaticos
+app.use(express.static(path.join(__dirname,'public')))
+
+
+// Stating the server ==> inicio del servidor
+
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`);
 })
---
+++ src/models/database.js
@@ -0,0 +1,11 @@
+// archivo para conectar a la base de datos
+
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/mern-task';
+
+mongoose.connect(URI)
+.then(db => console.log('DB is connected'))
+.catch(err =>console.log(err))
+
+module.exports = mongoose;
